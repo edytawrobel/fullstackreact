5add5215b7b7098a219bb91f7544cd4efc36ec10
Edyta Wrobel <ewrobel@thoughtworks.com>
2018-08-10 11:44:15 +0100

    object spread operator, add remove and edit expense on expensesReducer

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,8 +1,31 @@
 import { createStore, combineReducers } from 'redux';
+import uuid from 'uuid';
 
+// Action Generators
 // ADD_EXPENSE
+const addExpense = ({ description = '', note = '', amount = 0, createdAt = 0} = {}) => ({
+    type: 'ADD_EXPENSE',
+    expense: {
+        id: uuid(),
+        description,
+        note,
+        amount,
+        createdAt
+    }
+});
+
 // REMOVE_EXPENSE
+
+const removeExpense = ( { id } = {}) => ({
+    type: 'REMOVE_EXPENSE',
+    id
+})
 // EDIT_EXPENSE
+const editExpense = (id, updates = {}) => ({
+    type: 'EDIT_EXPENSE',
+    id,
+    updates
+})
 // SET_TEXT_FILTER
 // SORT_BY_DATE
 // SORT_BY_AMOUNT
@@ -13,9 +36,31 @@ import { createStore, combineReducers } from 'redux';
 const expensesReducerDefaultState = [];
 const expensesReducer = ( state = expensesReducerDefaultState, action ) => {
     switch (action.type) {
+        case 'ADD_EXPENSE' :
+           return [
+               ...state,   // spread operator, all expenses
+               action.expense
+           ];
+        case 'REMOVE_EXPENSE' :
+            // return state.filter((expense) => {
+            //     expense !== action.id // expense was a match and needs to get filtered out
+            // });
+
+            return state.filter(( { id }) => id !== action.id);
+        case 'EDIT_EXPENSE' :
+           return state.map((expense) => {
+              if (expense.id === action.id) {
+                return {
+                    ...expense,
+                    ...action.updates
+                }
+              } else {
+                  return expense
+              }
+           });
         default:
             return state;
-    }
+    };
 }; 
 
 //Filters reducer
@@ -29,15 +74,22 @@ const filtersReducer = (state = filtersReducerDefaultState, action) => {
 }
 
 // Store creation
-
 const store = createStore(
     combineReducers({
         expenses: expensesReducer,
-        filters: filtersReducer  // rotes
+        filters: filtersReducer  // routes
     })
 );
 
-console.log(store.getState());
+//notify about state changes
+store.subscribe(() => {
+    console.log(store.getState());
+})
+
+const expenseOne = store.dispatch(addExpense( { description: 'Rent', amount: 500 } ));
+const expenseTwo = store.dispatch(addExpense( { description: 'Coffee', amount: 100} ));
+store.dispatch(removeExpense( { id: expenseOne.expense.id } ));
+store.dispatch(editExpense(expenseTwo.expense.id, { amount: 200 }));
 
 const demoState = {
     expenses: [ {
