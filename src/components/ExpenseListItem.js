250bea3164a7f769d07e10164105cc98a7ce683c
Edyta Wrobel <ewrobel@thoughtworks.com>
2018-08-18 14:38:07 +0100

    writing to the store from a single component; use dispatch with action generators from a handler through recat-redux connect

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import expenses from '../selectors/expenses';
 import { connect } from 'react-redux';
+import { removeExpense } from '../actions/expenses';
 
-const ExpenseListItem = ({ description, amount, createdAt }) => (
+const ExpenseListItem = ({ dispatch, id, description, amount, createdAt }) => (
     <div>
         <h4>Description: { description }</h4>
         <p>Amount: { amount }</p>
         <p>Created At: { createdAt }</p>
+        <button onClick={ () => { dispatch(removeExpense({id})) } }>Remove</button>
     </div>
 );
 
-export default ExpenseListItem;
+export default connect()(ExpenseListItem);
