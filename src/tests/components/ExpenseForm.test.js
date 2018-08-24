e8dd84faa03d30b628947678a7494a55b04ed854
Edyta Wrobel <ewrobel@thoughtworks.com>
2018-08-24 19:11:31 +0100

    test expense form shallow

--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -3,9 +3,12 @@ import { shallow } from 'enzyme';
 import ExpenseForm from '../../components/ExpenseForm';
 import expenses from '../fixtures/expenses';
 
-
 test('should render ExpenseForm', () => {
     const wrapper = shallow(<ExpenseForm />);
     expect(wrapper).toMatchSnapshot();
 })
 
+test('should render ExpenseForm with expense data', () => {
+    const wrapper = shallow(<ExpenseForm expense={expenses[0]}/>);
+    expect(wrapper).toMatchSnapshot();
+})
