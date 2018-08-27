d19f037be3e9863ddb880c7f7edf57a0af656721
Edyta Wrobel <ewrobel@thoughtworks.com>
2018-08-27 15:13:28 +0100

    Test EditExpensePage; use mapDispatchToProps, refactor to be a class component

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -3,17 +3,27 @@ import ExpenseForm from './ExpenseForm';
 import { connect } from 'react-redux';
 import { addExpense } from '../actions/expenses';
 
-const AddExpensePage = (props) => (
-    <div>
-        <h2>Add Expense</h2>
-        <ExpenseForm 
-            onSubmit={ (expense) => {
-                props.dispatch(addExpense(expense));
-                props.history.push('/');
-                console.log(expense);
-            }}
-        />
-    </div>
-)
+export class AddExpensePage extends React.Component {
+    
+    onSubmit = (expense) => {
+        this.props.addExpense(expense);
+        this.props.history.push('/');
+    };
 
-export default connect()(AddExpensePage);
+    render() {
+        return (
+            <div>
+                <h2>Add Expense</h2>
+                <ExpenseForm 
+                    onSubmit={ this.onSubmit }
+                />
+            </div>
+        );
+    }
+}
+
+const mapDispatchToProps = (dispatch) => ({
+    addExpense: (expense) => dispatch(addExpense(expense))   
+});
+    
+export default connect(undefined, mapDispatchToProps)(AddExpensePage);
