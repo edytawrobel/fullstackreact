c6d9b0e23470f0db65c2851c5429925d93c501a7
Edyta Wrobel <ewrobel@thoughtworks.com>
2018-08-27 18:48:02 +0100

    Finalise testing of ExpenseListFilters

--- src/tests/components/ExpenseListFilters.test.js
+++ src/tests/components/ExpenseListFilters.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { ExpenseListFilters } from '../../components/ExpenseListFilters';
 import { filters, altFilters } from '../fixtures/filters';
+import moment from 'moment';
 
 let setTextFilter, sortByDate, sortByAmount, setStartDate, setEndDate, wrapper;
 
@@ -30,4 +31,45 @@ test('should render ExpenseListFilters with alt data correctly', () => {
         filters: altFilters
     })
     expect(wrapper).toMatchSnapshot();
+})
+
+test('should handle text change', () => {
+    const value = 'new text';
+    wrapper.find('input').simulate('change', {
+        target: { value }
+    })
+    expect(setTextFilter).toHaveBeenLastCalledWith(value);
+})
+
+test('should sort by date', () => {
+    const value = 'date';
+    wrapper.setProps({
+        filters: altFilters
+    })
+    wrapper.find('select').simulate('change', {
+        target: { value }
+    })
+    expect(sortByDate).toHaveBeenCalled();
+})
+
+test('should sort by amount', () => {
+    const value = 'amount';
+    wrapper.find('select').simulate('change', {
+        target: { value }
+    })
+    expect(sortByAmount).toHaveBeenCalled();
+})
+
+test('should handle date changes', () => {
+    const startDate = moment(0);
+    const endDate = moment(0).add(1, 'day');
+    wrapper.find('DateRangePicker').prop('onDatesChange')({startDate, endDate}); // it expects to be called with moment instance
+    expect(setStartDate).toHaveBeenLastCalledWith(startDate);
+    expect(setEndDate).toHaveBeenLastCalledWith(endDate);
+})
+
+test('should handle date focus changes', () => {
+    const calendarFocused = 'endDate';
+    wrapper.find('DateRangePicker').prop('onFocusChange')(calendarFocused);
+    expect(wrapper.state('calendarFocused')).toBe(calendarFocused);
 })
